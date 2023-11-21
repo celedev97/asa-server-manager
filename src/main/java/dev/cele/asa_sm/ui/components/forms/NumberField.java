797a81a082c65903638186bdb5d1a38965a69e03
celedev97 <celedev97@gmail.com>
2023-11-21 17:16:06 +0100

    rework of number field and slider

--- src/main/java/dev/cele/asa_sm/ui/components/forms/NumberField.java
+++ src/main/java/dev/cele/asa_sm/ui/components/forms/NumberField.java
@@ -10,12 +10,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-public class NumberField extends JSpinner {
+public class NumberField extends JTextField {
 
     private final ArrayList<Consumer<Number>> numberListeners = new ArrayList<>();
 
-    public Number getNumber() {
-        return (Number) getValue();
+    @Getter
+    private Number number = 0;
+
+    @Getter
+    private boolean isInteger = true;
+    public void setIsInteger(boolean isInteger){
+        this.isInteger = isInteger;
+        if(isInteger){
+            this.number = number.intValue();
+        }
+        validateTextAndUpdate(getText());
+    }
+
+    public void setNumber(Number number){
+        this.number = number;
+        setText(number.toString());
     }
 
     public NumberField() {
@@ -23,15 +37,24 @@ public class NumberField extends JSpinner {
     }
 
     public NumberField(Number value) {
-        super();
-        JSpinner.NumberEditor editor = new JSpinner.NumberEditor(this, "#");
-        setEditor(editor);
+        super(5);
 
-        setValue(value);
-
-        addChangeListener(e -> {
-            numberListeners.forEach(l -> l.accept((Number) getValue()));
+        //add focus lost listener
+        addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                validateTextAndUpdate(getText());
+            }
         });
+    }
+
+    private void validateTextAndUpdate(String text){
+        try {
+            number = isInteger ? Integer.parseInt(text) : Double.parseDouble(text);
+            numberListeners.forEach(listener -> listener.accept(number));
+        } catch (NumberFormatException e) {
+            //TODO: make a beep?
+            setText(number.toString());
+        }
     }
 
     public void addNumberListener(Consumer<Number> listener){
--- src/main/java/dev/cele/asa_sm/ui/components/forms/SliderWithText.java
+++ src/main/java/dev/cele/asa_sm/ui/components/forms/SliderWithText.java
@@ -1,12 +1,80 @@
 package dev.cele.asa_sm.ui.components.forms;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 import java.util.function.Consumer;
 
+@Slf4j
 public class SliderWithText extends JPanel {
     private final JSlider slider;
     private final NumberField numberField;
+
+    @Getter
+    private Number min = 0;
+    @Getter
+    private Number max = 100;
+    @Getter
+    private Number value = 0;
+    @Getter
+    private Number step = 1;
+
+    public void setMin(Number min) {
+        this.min = min;
+        updateSlider();
+    }
+
+    public void setMax(Number max) {
+        this.max = max;
+        updateSlider();
+    }
+
+    public void setValue(Number value) {
+        this.value = value;
+        updateSlider();
+    }
+
+    public void setStep(Number step) {
+        this.step = step;
+        if(step.intValue() != step.doubleValue()){
+            numberField.setIsInteger(false);
+        }
+        updateSlider();
+    }
+
+    public void set(Number min, Number max, Number step){
+        this.min = min;
+        this.max = max;
+        this.step = step;
+        updateSlider();
+    }
+
+    public void set(Number min, Number max, Number step, Number value) {
+        setValue(value);
+        set(min, max, step);
+    }
+
+
+    private void updateSlider(){
+        var stepMultiplier = 1 / step.doubleValue();
+
+        slider.setMinimum((int)(stepMultiplier));
+        slider.setMaximum((int)(max.doubleValue() * stepMultiplier));
+        slider.setValue((int)(value.doubleValue() * stepMultiplier));
+
+        if(stepMultiplier > 1){
+            log.info("OR, min: {}, max: {}, step: {}", min, max, step);
+            log.info("ED, min: {}, max: {}, step: {}", slider.getMinimum(), slider.getMaximum(), 1);
+        }
+
+        slider.setMajorTickSpacing(1);
+        slider.setMinorTickSpacing(1);
+    }
+
 
     public SliderWithText(){
         this(0, 100, 0);
@@ -14,6 +82,14 @@ public class SliderWithText extends JPanel {
 
     public SliderWithText(int min, int max, int value) {
         slider = new JSlider(min, max, value);
+
+        this.min = min;
+        this.max = max;
+        this.value = value;
+        this.step = 1;
+
+        updateSlider();
+
         numberField = new NumberField(value);
 
         setLayout(new BorderLayout());
@@ -21,14 +97,17 @@ public class SliderWithText extends JPanel {
         add(numberField, BorderLayout.EAST);
 
         slider.addChangeListener(e -> {
-            if (numberField.getNumber().doubleValue() != (double)slider.getValue()) {
-                numberField.setValue(slider.getValue());
+            if(!Objects.equals(this.value, slider.getValue())){
+                var realValue = slider.getValue() * step.doubleValue();
+                log.info("Slider value: {}, real value: {}", slider.getValue(), realValue);
+                numberField.setNumber(realValue);
             }
         });
 
         numberField.addNumberListener(val -> {
-            if (slider.getValue() != val.intValue()) {
-                slider.setValue(val.intValue());
+            if(!Objects.equals(this.value, val)){
+                this.value = val;
+                slider.setValue((int)(val.doubleValue() * (1 / step.doubleValue())));
             }
         });
 
@@ -37,4 +116,5 @@ public class SliderWithText extends JPanel {
     public void addChangeListener(Consumer<Number> listener) {
         numberField.addNumberListener(listener);
     }
+
 }
